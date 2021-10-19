9dc66c89d3d87622b7a665fa9932787a76c456c0
Ratmir <foxratmir2000@gmail.com>
2021-10-19 19:22:24 +0200

    Implement 17SoftHitRule

---
+++ src/main/java/model/rules/Soft17HitStrategy.java
@@ -0,0 +1,18 @@
+package model.rules;
+
+import model.Card;
+import model.Player;
+
+class Soft17HitStrategy implements HitStrategy {
+  private static final int hitLimit = 17;
+
+  public boolean doHit(Player dealer) {
+
+    for(Card temp: dealer.getHand()){
+      if(temp.getValue().equals(Card.Value.Ace) && dealer.calcScore() == hitLimit){
+        return dealer.calcScore() - 10 < hitLimit;
+      }
+    }
+    return dealer.calcScore() < hitLimit;
+  }
+}
